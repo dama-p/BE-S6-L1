4f41d80d482a5e34f9f85c63beae582dea016084
Damaride Putignano <damarideputignano@gmail.com>
2024-05-16 15:24:09 +0200

    First Commit

---
+++ app/Http/Controllers/BooksController.php
@@ -0,0 +1,27 @@
+<?php
+
+namespace App\Http\Controllers;
+
+use Illuminate\Http\Request;
+
+class BooksController extends Controller
+{
+    public function getBooks()
+    {
+        return view('booklist');
+    }
+    public function getDetails($id)
+    {
+        return view('details');
+    }
+
+    public function addBook()
+    {
+        return view('add');
+    }
+
+    public function editBook()
+    {
+        return view('edit');
+    }
+}
---
+++ routes/web.php
@@ -0,0 +1,17 @@
+<?php
+
+use Illuminate\Support\Facades\Route;
+use App\Http\Controllers\BooksController;
+
+
+Route::get('/', function () {
+    return view('welcome');
+});
+
+Route::get('/books', [BooksController::class,'getBooks'])->name('getBooks');
+
+Route::get('/books/{id}', [BooksController::class,'getDetails'])->name('getDetails');
+
+Route::get('/add', [BooksController::class,'addBook'])->name('addBook');
+
+Route::get('/edit', [BooksController::class,'editBook'])->name('editBook'); 
